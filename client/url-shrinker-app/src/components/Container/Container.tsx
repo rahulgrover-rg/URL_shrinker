0be4643ebad73b4aed58a2c3d2934eee571fc2e5
rahulgrover-rg <rahulgrover9373@gmail.com>
2024-11-10 20:08:04 +0530

    Add functionality

--- client/url-shrinker-app/src/components/Container/Container.tsx
+++ client/url-shrinker-app/src/components/Container/Container.tsx
@@ -1,13 +1,36 @@
 import * as React from 'react';
-import FormContainer from '../FormContainer/FormContainer';
+import { UrlData } from '../../interface/UrlData';
+import FormContainer from '../FormContainer/FormContainer';import axios from 'axios' ;
+import { serverUrl } from '../../helpers/Constants';
+import DataTable from '../DataTable/DataTable';
 
 interface IContainerProps {
 }
 
 const Container: React.FunctionComponent<IContainerProps> = () => {
+
+  const [data,setData] = React.useState<UrlData[]>([]) ;
+  const [reload,setReload] = React.useState<boolean>(false) ;
+
+  const fetchTableData = async() => {
+     const response = await axios.get(`${serverUrl}/shortUrl`) ;
+     console.log("The response from sever is :" ,response) ;
+     setData(response.data) ;
+     setReload(false) ;
+  }
+
+  const updateReload = ():void => {
+    setReload(true) ;
+  }
+
+  React.useEffect(()=> {
+    fetchTableData() ;
+  },[reload]) ;
+
   return (
     <>
-        <FormContainer />
+        <FormContainer updateReload={updateReload}/>
+        <DataTable data={data} updateReload={updateReload}/>
     </>
   );
 };
